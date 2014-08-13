e9231192f5ce7f1cefc993916ff06d2206c4773c
git <jonhemstreet@gmail.com>
2014-08-12 19:32:35 -0600

    fix push

--- web/public/js/GameController.js
+++ web/public/js/GameController.js
@@ -8,12 +8,12 @@ pongpi.controller('GameController', function ($scope) {
     $scope.joined       = false;
     $scope.colors       = ['#dfd9b5', '#a4897e', '#ff625d', '#00525e', '#1a303b', '#5a3735'];
 
-//    altair.sockets.on('score', function (e) {
-//
-//        $scope.player.score += e.get('points');
-//        $scope.$apply();
-//
-//    });
+    altair.sockets.on('score', function (e) {
+
+        $scope.player.score += e.get('points');
+        $scope.$apply();
+
+    });
 
 
     onScroll = function () {
@@ -21,13 +21,12 @@ pongpi.controller('GameController', function ($scope) {
         var max     = $('.paddle-scroll').height() - $('.game-board').height(),
             percent = $('.game-board').scrollTop() / max;
 
-        console.log('max', max, percent);
 
         if ($scope.joined && max > 0) {
 
-//            altair.sockets.emit('scroll', {
-//                distance: percent
-//            });
+            altair.sockets.emit('scroll', {
+                distance: percent
+            });
         }
 
     };
@@ -37,17 +36,17 @@ pongpi.controller('GameController', function ($scope) {
         //pick a side
         $scope.side = side;
 
-//        altair.sockets.emit('picked-side', {
-//            side: side
-//        });
+        altair.sockets.emit('picked-side', {
+            side: side
+        });
 
     };
 
     $scope.enterUsername = function (username) {
 
-//        altair.sockets.emit('enter-username', {
-//            username: username
-//        });
+        altair.sockets.emit('enter-username', {
+            username: username
+        });
 
         $scope.player = {
             username: username,
@@ -62,16 +61,16 @@ pongpi.controller('GameController', function ($scope) {
 
         $scope.player.color = color;
 
-//        altair.sockets.emit('color', {
-//            color: color
-//        });
+        altair.sockets.emit('color', {
+            color: color
+        });
 
         this.join();
     };
 
     $scope.join = function () {
 
-//        altair.sockets.emit('join');
+        altair.sockets.emit('join');
         $scope.joined = true;
 
         var max     = $('.paddle-scroll').height() - $('.game-board').height();
